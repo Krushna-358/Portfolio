59ab5fd005866b85a8d819c6886b126f07fd28d5
Krushna <krushnababhulkar93@gmail.com>
2025-02-14 13:31:06 +0530

    Portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Export static files
-    basePath: '/YourRepoName', // Replace with your GitHub repo name
-    assetPrefix: '/YourRepoName/', // Same as basePath
+    basePath: '/Portfolio', // Replace with your GitHub repo name
+    assetPrefix: '/Portfolio/', // Same as basePath
     images: {
       unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
     },
